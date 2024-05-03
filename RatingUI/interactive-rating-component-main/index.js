63c2b5fa1dfd883cd1fe45b31db9a86437400a56
MUKESH <161928235+Mukesh-0304@users.noreply.github.com>
2024-05-03 13:09:05 +0530

    Add files via upload

---
+++ RatingUI/interactive-rating-component-main/index.js
@@ -0,0 +1,67 @@
+function get(){
+    document.getElementById("one1").style.backgroundColor = "grey";
+    document.getElementById("one1").style.fontWeight = "bolder";
+    document.getElementById("one1").style.color = "white";   
+    document.getElementById("one2").style.backgroundColor = "";
+    document.getElementById("one3").style.backgroundColor = "";
+    document.getElementById("one4").style.backgroundColor = "";
+    document.getElementById("one5").style.backgroundColor = "";
+    document.getElementById("one2").style.Color = "grey";
+    document.getElementById("one3").style.Color = "grey";
+    document.getElementById("one4").style.Color = "grey";
+    document.getElementById("one5").style.Color = "grey";
+}
+
+function get1(){
+    document.getElementById("one2").style.backgroundColor = "grey";
+    document.getElementById("one2").style.fontWeight = "bolder"
+    document.getElementById("one2").style.color = "white";
+    document.getElementById("one1").style.backgroundColor = "";
+    document.getElementById("one3").style.backgroundColor = "";
+    document.getElementById("one4").style.backgroundColor = "";
+    document.getElementById("one5").style.backgroundColor = "";
+    document.getElementById("one1").style.Color = "";
+    document.getElementById("one3").style.Color = "";
+    document.getElementById("one4").style.Color = "";
+    document.getElementById("one5").style.Color = "";
+}
+function get2(){
+    
+    document.getElementById("one3").style.backgroundColor = "grey";
+    document.getElementById("one3").style.fontWeight = "bolder";
+    document.getElementById("one3").style.color = "white";  
+    document.getElementById("one2").style.backgroundColor = "";
+    document.getElementById("one1").style.backgroundColor = "";
+    document.getElementById("one4").style.backgroundColor = "";
+    document.getElementById("one5").style.backgroundColor = ""; 
+    document.getElementById("one2").style.Color = "";
+    document.getElementById("one1").style.Color = "";
+    document.getElementById("one4").style.Color = "";
+    document.getElementById("one5").style.Color = "";
+}
+function get3(){
+    document.getElementById("one4").style.backgroundColor = "grey";
+    document.getElementById("one4").style.fontWeight = "bolder";
+    document.getElementById("one4").style.color = "white";   
+    document.getElementById("one2").style.backgroundColor = "";
+    document.getElementById("one3").style.backgroundColor = "";
+    document.getElementById("one1").style.backgroundColor = "";
+    document.getElementById("one5").style.backgroundColor = "";
+    document.getElementById("one2").style.Color = "";
+    document.getElementById("one3").style.Color = "";
+    document.getElementById("one1").style.Color = "";
+    document.getElementById("one5").style.Color = "";
+}
+function get4(){
+    document.getElementById("one5").style.backgroundColor = "grey";
+    document.getElementById("one5").style.fontWeight = "bolder";
+    document.getElementById("one5").style.color = "white";   
+    document.getElementById("one2").style.backgroundColor = "";
+    document.getElementById("one3").style.backgroundColor = "";
+    document.getElementById("one4").style.backgroundColor = "";
+    document.getElementById("one1").style.backgroundColor = "";
+    document.getElementById("one2").style.Color = "";
+    document.getElementById("one3").style.Color = "";
+    document.getElementById("one4").style.Color = "";
+    document.getElementById("one1").style.Color = "";
+}
